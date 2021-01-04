df8d0334e8bddcf2e41d3d9ee6d00da3f1a416c7
Umar Khalilov <ERMASTER100@gmail.com>
2021-01-04 22:05:31 +0200

    Functions in class created

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 //Даны числа 4, -2, 5, 19, -130, 0, 10. Найдите минимальное и максимальное число.
-function getMinNum(num1, num2) {
-    const res = num2 - num1;
-    if (isNaN(res)) {
+function getMinNum() {
+    if (isNaN(arguments[0])) {
         return null;
     }
-    if (num1 > num2) {
-        return [num2];
+    let min = arguments[0];
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] < min) {
+            min = arguments[i];
+        }
     }
-    return [num1];
+    return min;
 }
 
-function getMaxNum(num1, num2) {
-    const res = num2 - num1;
-    if (isNaN(res)) {
+function getMaxNum() {
+    if (isNaN(arguments[0])) {
         return null;
     }
-    if (num1 > num2) {
-        return [num1];
+    let max = arguments[0];
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] > max) {
+            max = arguments[i]
+        }
     }
-    return [num2];
+    return max;
+}
+
+function getMinAndMaxNumbers() {
+    if (isNaN(arguments[0])) {
+        return null;
+    }
+    return [getMinNum(...arguments), getMaxNum(...arguments)]
 }
 
 //Дан массив arr. Найдите среднее арифметическое его элементов. 
 //Проверьте задачу на массиве с элементами 12, 15, 20, 25, 59, 79.
-arr1 = [12, 15, 20, 25, 59, 79];
+const arr1 = [12, 15, 20, 25, 59, 79];
 function getAverage(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
@@ -36,5 +47,14 @@ console.log(myArr);
 
 //Выведите на экран случайное целое число от 1 до 100.
 function getRandom(numBegin, numEnd) {
-    return Math.ceil(Math.random() * (numBegin + numEnd) + numBegin);
+    return Math.ceil(Math.random() * (numEnd - numBegin) + numBegin);
 }
+console.log(getRandom(1, 100));
+
+//Заполните массив 10-ю случайными целыми числами.
+const myArray = [];
+let numberLength = 10;
+for (let i = 0; i < numberLength; i++) {
+    myArray.push(Math.round(Math.random() * numberLength));
+}
+console.log(myArray);
